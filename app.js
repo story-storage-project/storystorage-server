d4721fda9c57ddd83d1dae17bf5a33cf53e847d8
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-11-24 19:27:23 +0900

    Feat cookieparser 적용, 라우터 추가

--- app.js
+++ app.js
@@ -1,46 +1,51 @@
-import './connectEnv.js';
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import cors from 'cors';
+import config from './config.js';
 import connectDB from './db/database.js';
-
-if (process.env.NODE_ENV !== 'test') {
-  connectDB();
-}
+import routes from './routes/index.js';
 
 const app = express();
 
-app.use(morgan('tiny'));
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: [config.origin],
+  }),
+);
 
 app.use(express.json());
+app.use(cookieParser());
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
-// app.use("/users/", storyRouter);
+app.use('/auth', routes.authRouter);
+app.use('/user', routes.userRouter);
+app.use('/users/:userId/stories', routes.storyRouter);
 
-app.use(function (req, res, next) {
-  const error = new Error('Page not found');
+app.use('*', (req, res, next) => {
+  const error = new Error(`Route ${req.originalUrl} not found`);
   error.status = 404;
-  error.message = 'Not Found';
+
   next(error);
 });
 
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   res.status(err.status || 500);
 
-  if (req.headers['content-type'] === 'application/json') {
-    res.json({
-      result: 'error',
-    });
-  } else {
-    if (res.status >= 500) {
-      res.json({ message: 'Internal Server Error' });
-      return;
-    }
-    res.json({ message: 'error' });
+  if (res.status >= 500) {
+    return res.json({ message: 'Internal Server Error' });
   }
+  return res.json({ message: res.locals.message });
 });
 
-app.listen(process.env.PORT);
+app.listen(config.serverPort, () => {
+  console.log(`Server started on port: ${config.serverPort}`);
+
+  if (process.env.NODE_ENV !== 'test') {
+    connectDB();
+  }
+});
