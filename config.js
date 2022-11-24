d1e05d1681e31f77dfa4422352f614cbd71fa475
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-11-24 19:27:51 +0900

    Feat config 추가

---
+++ config.js
@@ -0,0 +1,39 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+function required(key, defaultValue = undefined) {
+  const value = process.env[key] || defaultValue;
+  if (value == null) {
+    throw new Error(`Key ${key} is undefined`);
+  }
+  return value;
+}
+
+export default {
+  serverPort: parseInt(required('PORT', 8000), 10),
+  origin: required('ORIGIN'),
+
+  db: {
+    id: required('DB_ID'),
+    name: required('DB_NAME'),
+    password: required('DB_PASSWORD'),
+    cluster: required('DB_CLUSTER'),
+  },
+
+  token: {
+    accessTokenPrivateKey: required('ACCESS_TOKEN_PRIVATE_KEY'),
+    accessTokenPublicKey: required('ACCESS_TOKEN_PUBLIC_KEY'),
+    refreshTokenPrivateKey: required('REFRESH_TOKEN_PRIVATE_KEY'),
+    refreshTokenPublicKey: required('REFRESH_TOKEN_PUBLIC_KEY'),
+
+    accessTokenExpiresIn: required('ACCESS_TOKEN_EXPIRES_IN'),
+    refreshTokenExpiresIn: required('REFRESH_TOKEN_EXPIRES_IN'),
+  },
+
+  google: {
+    clientId: required('GOOGLE_OAUTH_CLIENT_ID'),
+    clientSecret: required('GOOGLE_OAUTH_CLIENT_SECRET'),
+    oAuthRedirect: required('GOOGLE_OAUTH_REDIRECT_URL'),
+  },
+};
