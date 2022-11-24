ceb84b8cf0c2f4875d42439c629bb99fa73fafff
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-11-24 19:32:06 +0900

    Chore axios, cookie-parser, jwt, qs 설치

--- db/database.js
+++ db/database.js
@@ -1,12 +1,17 @@
 import mongoose from 'mongoose';
+import config from '../config.js';
+
+const { db } = config;
+const DB_HOST = `mongodb+srv://${db.id}:${db.password}@cluster0.${db.cluster}.mongodb.net/${db.name}`;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_HOST);
+    await mongoose.connect(DB_HOST);
     console.log('Success DB Connection');
     return;
   } catch (err) {
     console.log(`Fail DB Connection. error: ${err}`);
+    setTimeout(connectDB, 5000);
   }
 };
 
