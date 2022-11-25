3f9c02fece0a3e500c5c72c6a896f9090ea1320e
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-11-25 10:33:59 +0900

    test

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ app.use(
   }),
 );
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
