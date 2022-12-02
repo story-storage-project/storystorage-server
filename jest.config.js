22bf3fa5472c8c8d6c0d9a743a5334172c857d0b
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-12-03 02:54:21 +0900

    Feat test 관련 설치

--- jest.config.js
+++ jest.config.js
@@ -145,7 +145,8 @@ export default {
 
   // The test environment that will be used for testing
   // testEnvironment: "jest-environment-node",
-  testEnvironment: 'node',
+  // testEnvironment: 'node',
+  testEnvironment: 'jest-environment-node',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -175,12 +176,10 @@ export default {
 
   // A map from regular expressions to paths to transformers
   // transform: undefined,
+  // transform: {},
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/",
-  //   "\\.pnp\\.[^\\/]+$"
-  // ],
+  // transformIgnorePatterns: ['/node_modules/', '\\.pnp\\.[^\\/]+$'],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
